779a643d59b2a8388a9dd4fa598e4db89a4091b2
“ahof1704” <ahof1704@gmail.com>
2019-08-28 09:24:58 -0400

    Convolutional_Autoenconder

---
+++ test.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 27 10:32:22 2019
+
+@author: antonio
+Just playing around with some autoenconders
+
+"""
+# matplotlib inline
+
+import torch
+import numpy as np
+from torchvision import datasets
+import torchvision.transforms as transforms
+
+# convert data to torch.FloatTensor
+transform = transforms.ToTensor()
+
+# load the training and test datasets
+train_data = datasets.MNIST(root='data', train=True,
+                                   download=True, transform=transform)
+test_data = datasets.MNIST(root='data', train=False,
+                                  download=True, transform=transform)
+
+# Create training and test dataloaders
+
+num_workers = 0
+# how many samples per batch to load
+batch_size = 20
+
+# prepare data loaders
+train_loader = torch.utils.data.DataLoader(train_data, batch_size=batch_size, num_workers=num_workers)
+test_loader = torch.utils.data.DataLoader(test_data, batch_size=batch_size, num_workers=num_workers)
+
+import matplotlib.pyplot as plt
+#matplotlib inline
+    
+# obtain one batch of training images
+dataiter = iter(train_loader)
+images, labels = dataiter.next()
+images = images.numpy()
+
+# get one image from the batch
+img = np.squeeze(images[0])
+
+fig = plt.figure(figsize = (5,5)) 
+ax = fig.add_subplot(111)
+ax.imshow(img, cmap='gray')
